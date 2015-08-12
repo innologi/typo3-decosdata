80ef7959e3bec76de1a40899dc5e9cb5b3640a0c
Frenck Lutke <git@innologi.nl>
2015-08-12 17:06:18 +0200

    [TASK] implement "Add Static TS"
    Change-Id: I97242bd5d74ee13b1774276d05227ca31c6cd1b0

--- ext_tables.php
+++ ext_tables.php
@@ -3,9 +3,12 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-
 $tablePrefix = 'tx_' . $_EXTKEY . '_domain_model_';
 $cshPathPrefix = 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_';
+
+// @TODO ___rename
+// add static TS
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'DecosPublisher7');
 
 // allow records of these tables to be stored on standard page types
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages($tablePrefix . 'profile');
