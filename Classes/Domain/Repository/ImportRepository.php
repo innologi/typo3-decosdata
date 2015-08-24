646b466cee026efb596f7c2836129547524e9b91
Frenck Lutke <git@innologi.nl>
2015-08-24 15:08:10 +0200

    [TASK] Add a pid-disrespecting findAll() method to importRepo
    Change-Id: I59a20e5c7f2856b4d5f51835f743f746ec14d61d

--- Classes/Domain/Repository/ImportRepository.php
+++ Classes/Domain/Repository/ImportRepository.php
@@ -1,7 +1,6 @@
 <?php
 namespace Innologi\Decospublisher7\Domain\Repository;
 /***************************************************************
- *
  *  Copyright notice
  *
  *  (c) 2015 Frenck Lutke <typo3@innologi.nl>, www.innologi.nl
@@ -24,10 +23,25 @@ namespace Innologi\Decospublisher7\Domain\Repository;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-use TYPO3\CMS\Extbase\Persistence\Repository;
+use Innologi\Decospublisher7\Mvc\Domain\RepositoryAbstract;
 /**
- * The repository for Items
+ * Import domain repository
+ *
+ * @package decospublisher7
+ * @author Frenck Lutke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
-class ImportRepository extends Repository {
+class ImportRepository extends RepositoryAbstract {
+
+	/**
+	 * Returns all objects of this repository regardless of pid.
+	 *
+	 * @return \TYPO3\CMS\Extbase\Persistence\QueryResultInterface|array
+	 */
+	public function findAllEverywhere() {
+		$query = $this->createQuery();
+		$query->getQuerySettings()->setRespectStoragePage(FALSE);
+		return $query->execute();
+	}
 
 }
