5ef36dd385007b6c8f5339f3d00b2a4414169c8a
Frenck Lutke <git@innologi.nl>
2015-08-25 17:04:32 +0200

    [TASK] Create Classic Storage Handler for importer
    Handles storage of parsed import via direct queries. Although this
    implementation has an enormous speed-benefit as opposed to other
    StorageHandlers, it will not update TYPO3-specific reference fields,
    with the exception of FileReferences. However, the extension doesn't
    really rely on them to function properly.
    
    This storage handler improves performance by a factor of 3.1 compared to
    the extbase storage handler, which can make all the difference for large
    import files.
    
    Change-Id: Ie9419d77551d91d78af2cc46e5b06e5fbf11cc34

---
+++ Classes/Service/Database/DatabaseHelper.php
@@ -0,0 +1,202 @@
+<?php
+namespace Innologi\Decospublisher7\Service\Database;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Frenck Lutke <typo3@innologi.nl>, www.innologi.nl
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Database\DatabaseConnection;
+use Innologi\Decospublisher7\Exception\SqlError;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+/**
+ * Database Helper
+ *
+ * Provides much used methods for constructing or executing database queries
+ * the non-FLOW way.
+ *
+ * @package decospublisher7
+ * @author Frenck Lutke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class DatabaseHelper implements SingletonInterface {
+
+	/**
+	 * @var \TYPO3\CMS\Core\Database\DatabaseConnection
+	 */
+	protected $databaseConnection;
+
+	/**
+	 * @var \Innologi\Decospublisher7\Service\Database\QueryProviderInterface
+	 * @inject
+	 */
+	protected $queryProvider;
+
+	/**
+	 * @var integer
+	 */
+	protected $lastUid = NULL;
+
+	/**
+	 * @var boolean
+	 */
+	protected $lastUpsertIsNewRecord = FALSE;
+
+	/**
+	 * Class constructor
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->databaseConnection = $GLOBALS['TYPO3_DB'];
+		$this->databaseConnection->store_lastBuiltQuery = TRUE;
+		// @TODO ___utilize $this->databaseConnection->sql_error() ?
+	}
+
+	/**
+	 * Execute an upsert, or InsertOrUpdateOnDuplicate: will attempt to insert a record,
+	 * or update en existing one if its data matches an existing record.
+	 *
+	 * @param string $table
+	 * @param array $data Contains property => value elements
+	 * @param array $uniqueProperties (optional)
+	 * @return object|boolean Query Result
+	 */
+	public function execUpsertQuery($table, array $data, array $uniqueProperties = array()) {
+		$this->lastUid = NULL;
+		$this->lastUpsertIsNewRecord = FALSE;
+		$res = $this->databaseConnection->sql_query(
+			$this->queryProvider->upsertQuery($table, $data, $uniqueProperties)
+		);
+
+		$uid = $this->databaseConnection->sql_insert_id();
+		if ($uid > 0) {
+			$this->lastUid = $uid;
+			$this->lastUpsertIsNewRecord = TRUE;
+		}
+
+		return $res;
+	}
+
+	/**
+	 * Inserts an MM table record, but only if it doesn't exist.
+	 *
+	 * @param string $table
+	 * @param integer $localUid
+	 * @param integer $foreignUid
+	 * @return void
+	 */
+	public function insertMmRelationIfNotExists($table, $localUid, $foreignUid) {
+		if (!$this->doesMmMatchExist($table, $localUid, $foreignUid)) {
+			$this->databaseConnection->exec_INSERTquery($table, array(
+				'uid_local' => $localUid,
+				'uid_foreign' => $foreignUid
+			));
+		}
+	}
+
+
+	/**
+	 * Creates a where string from a condition array
+	 *
+	 * @param array $conditions Contains property => value combinations
+	 * @param string $table Table from which the conditions originate
+	 * @return string
+	 */
+	public function getWhereFromConditionArray(array $conditions, $table = NULL) {
+		$where = array();
+		foreach ($conditions as $property => $value) {
+			$where[] = $property . '=' . $this->databaseConnection->fullQuoteStr($value, $table);
+		}
+		return join(' ' . DatabaseConnection::AND_Constraint . ' ', $where);
+	}
+
+	/**
+	 * Returns uid of newest record that matches $data parameters.
+	 *
+	 * @param string $table
+	 * @param array $data
+	 * @return integer|NULL
+	 * @throws \Innologi\Decospublisher7\Exception\SqlError
+	 */
+	public function getLastUidOfMatch($table, array $data) {
+		$row = $this->databaseConnection->exec_SELECTgetSingleRow(
+			'uid',
+			$table,
+			$this->getWhereFromConditionArray($data),
+			'',
+			'uid DESC'
+		);
+
+		if ($row === NULL) {
+			throw new SqlError(NULL, array(
+				$this->databaseConnection->debug_lastBuiltQuery
+			));
+		}
+		$this->lastUid = $row === FALSE
+			? NULL
+			: (int) $row['uid'];
+
+		return $this->lastUid;
+	}
+
+	/**
+	 * Returns whether an MM relation match exists.
+	 *
+	 * @param string $table
+	 * @param integer $localUid
+	 * @param integer $foreignUid
+	 * @return boolean
+	 */
+	public function doesMmMatchExist($table, $localUid, $foreignUid) {
+		$row = $this->databaseConnection->exec_SELECTgetSingleRow(
+			'*',
+			$table,
+			$this->getWhereFromConditionArray(array(
+				'uid_local' => $localUid,
+				'uid_foreign' => $foreignUid
+			)),
+			'',
+			'uid_local DESC,uid_foreign DESC'
+		);
+
+		return is_array($row);
+	}
+
+	/**
+	 * Returns last uid stored by any exec*() or getLastUid*() methods of this class.
+	 *
+	 * @return integer|NULL
+	 */
+	public function getLastUid() {
+		return $this->lastUid;
+	}
+
+	/**
+	 * Returns whether the last executed upsert query resulted in a new record.
+	 *
+	 * @return boolean
+	 */
+	public function getLastUpsertIsNewRecord() {
+		return $this->lastUpsertIsNewRecord;
+	}
+
+}
---
+++ Classes/Service/Importer/StorageHandler/ClassicStorageHandler.php
@@ -0,0 +1,330 @@
+<?php
+namespace Innologi\Decospublisher7\Service\Importer\StorageHandler;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Frenck Lutke <typo3@innologi.nl>, www.innologi.nl
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use Innologi\Decospublisher7\Exception\SqlError;
+/**
+ * Importer Storage Handler: Classic Edition
+ *
+ * Handles storage of parsed import via direct queries. Although this implementation
+ * has an enormous speed-benefit as opposed to other StorageHandlers, it will not
+ * update TYPO3-specific reference fields, with the exception of FileReferences.
+ * However, the extension doesn't really rely on them to function properly.
+ *
+ * @package decospublisher7
+ * @author Frenck Lutke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class ClassicStorageHandler implements StorageHandlerInterface,SingletonInterface {
+
+	/**
+	 * @var \TYPO3\CMS\Core\Database\DatabaseConnection
+	 */
+	protected $databaseConnection;
+
+	/**
+	 * @var \Innologi\Decospublisher7\Service\Database\DatabaseHelper
+	 * @inject
+	 */
+	protected $databaseHelper;
+
+	/**
+	 * @var \Innologi\Decospublisher7\Domain\Repository\FileReferenceRepository
+	 * @inject
+	 */
+	protected $fileReferenceRepository;
+
+	/**
+	 * @var array
+	 */
+	protected $propertyDefaults = array();
+
+	/**
+	 * @var array
+	 */
+	protected $itemFields = array();
+
+	/**
+	 * @var array
+	 */
+	protected $itemTypeCache = array();
+
+	/**
+	 * @var array
+	 */
+	protected $fieldCache = array();
+
+	/**
+	 * Class constructor
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->propertyDefaults = array(
+			'pid' => 1,
+			'crdate' => $GLOBALS['EXEC_TIME'],
+			'tstamp' => $GLOBALS['EXEC_TIME'],
+			'deleted' => 0
+			// leave enableFields alone, so that an import/update won't affect them
+		);
+		$this->databaseConnection = $GLOBALS['TYPO3_DB'];
+		$this->databaseConnection->store_lastBuiltQuery = TRUE;
+		// @TODO ___utilize $this->databaseConnection->sql_error() ?
+	}
+
+	/**
+	 * Initialize Storage Handler
+	 *
+	 * This will allow the importer to set specific parameters
+	 * that are of importance.
+	 *
+	 * @param integer $pid
+	 * @return void
+	 */
+	public function initialize($pid) {
+		$this->propertyDefaults['pid'] = $pid;
+		$this->fileReferenceRepository->setStoragePid($pid);
+	}
+
+	/**
+	 * Push an item ready for commit.
+	 *
+	 * @param array $data
+	 * @return integer
+	 */
+	public function pushItem(array $data) {
+		$table = 'tx_decospublisher7_domain_model_item';
+		$insert = array_merge(
+			$this->propertyDefaults,
+			array(
+				'item_key' => $data['item_key'],
+				'item_type' => $this->getItemTypeUid($data['item_type'])
+			)
+		);
+
+		$this->databaseHelper->execUpsertQuery($table, $insert, array('pid', 'item_key'));
+		$uid = $this->databaseHelper->getLastUid();
+		if ($uid === NULL) {
+			$uid = $this->databaseHelper->getLastUidOfMatch($table, array(
+				'pid' => $insert['pid'],
+				'item_key' => $insert['item_key']
+			));
+		}
+
+		// retrieves any previously stored itemFields of updated item or none if inserted
+		$this->itemFields = $this->databaseHelper->getLastUpsertIsNewRecord()
+			? array()
+			: $this->getItemFields($uid, $insert['pid']);
+
+		// push import-relation
+		$table = 'tx_decospublisher7_item_import_mm';
+		/* @var $import \Innologi\Decospublisher7\Domain\Model\Import */
+		foreach ($data['import'] as $import) {
+			$this->databaseHelper->insertMmRelationIfNotExists($table, $uid, $import->getUid());
+		}
+
+		// push parent-relation
+		if ($data['parent_item'] !== NULL) {
+			$table = 'tx_decospublisher7_item_item_mm';
+			$this->databaseHelper->insertMmRelationIfNotExists($table, $uid, (int) $data['parent_item']);
+		}
+
+		return $uid;
+	}
+
+	/**
+	 * Push an itemblob ready for commit.
+	 *
+	 * @param array $data
+	 * @return void
+	 */
+	public function pushItemBlob(array $data) {
+		$filePath = $data['filepath'];
+		unset($data['filepath']);
+
+		$table = 'tx_decospublisher7_domain_model_itemblob';
+		$data = array_merge($this->propertyDefaults, $data);
+		$this->databaseHelper->execUpsertQuery($table, $data, array('pid', 'item_key'));
+		$uid = $this->databaseHelper->getLastUid();
+		if ($uid === NULL) {
+			$uid = $this->databaseHelper->getLastUidOfMatch($table, array(
+				'pid' => $data['pid'],
+				'item_key' => $data['item_key']
+			));
+		}
+
+		$this->pushFileReference($filePath, $table, $uid, 'file');
+	}
+
+	/**
+	 * Push an itemfield ready for commit.
+	 *
+	 * @param array $data
+	 * @return void
+	 */
+	public function pushItemField(array $data) {
+		$table = 'tx_decospublisher7_domain_model_itemfield';
+		$data = array_merge($this->propertyDefaults, $data);
+		$data['field'] = $this->getFieldUid($data['field']);
+
+		$whereData = array(
+			'field' => $data['field'],
+			'item' => $data['item'],
+			'pid' => $data['pid']
+		);
+
+		if ($data['field_value'] === NULL) {
+			// if fieldvalue is NULL while the field previously existed with a value for this item, remove it
+			if (isset($this->itemFields[$data['field']])) {
+				$data['deleted'] = 1;
+				$this->databaseConnection->exec_UPDATEquery(
+					$table,
+					$this->databaseHelper->getWhereFromConditionArray($whereData, $table),
+					$data
+				);
+			}
+			// otherwise, just don't register it
+		} else {
+			// if we have a field value, always upsert
+			$this->databaseHelper->execUpsertQuery($table, $data, array_keys($whereData));
+		}
+	}
+
+	/**
+	 * Push a file reference ready for commit.
+	 *
+	 * @param string $filePath
+	 * @param string $foreignTable
+	 * @param integer $foreignUid
+	 * @param string $foreignField
+	 * @return void
+	 */
+	protected function pushFileReference($filePath, $foreignTable, $foreignUid, $foreignField) {
+		$this->fileReferenceRepository->upsertRecordByFilePath($filePath, $foreignTable, $foreignUid, $foreignField);
+	}
+
+	/**
+	 * Commits all pushed data.
+	 *
+	 * @return void
+	 */
+	public function commit() {
+		// doesn't do anything in this implementation
+	}
+
+	/**
+	 * Returns itemType uid by itemType string
+	 *
+	 * @param string $type
+	 * @return integer
+	 */
+	protected function getItemTypeUid($type) {
+		if (!isset($this->itemTypeCache[$type])) {
+			$this->itemTypeCache[$type] = $this->produceValueObjectUid(
+				'tx_decospublisher7_domain_model_itemtype',
+				array('item_type' => $type)
+			);
+		}
+		return $this->itemTypeCache[$type];
+	}
+
+	/**
+	 * Returns field uid by fieldname string
+	 *
+	 * @param string $field
+	 * @return integer
+	 */
+	protected function getFieldUid($field) {
+		if (!isset($this->fieldCache[$field])) {
+			$this->fieldCache[$field] = $this->produceValueObjectUid(
+				'tx_decospublisher7_domain_model_field',
+				array('field_name' => $field)
+			);
+		}
+		return $this->fieldCache[$field];
+	}
+
+	/**
+	 * Produces an uid of a valueobject record, either by retrieving
+	 * an existing record or inserting a new one.
+	 *
+	 * @param string $table
+	 * @param array $data Contains value-defining property => value
+	 * @return integer
+	 */
+	protected function produceValueObjectUid($table, array $data) {
+		$data['pid'] = $this->propertyDefaults['pid'];
+		$data['deleted'] = 0;
+
+		// first see if it exists at all (and not deleted)
+		$uid = $this->databaseHelper->getLastUidOfMatch($table, $data);
+		if ($uid === NULL) {
+			$this->databaseConnection->exec_INSERTquery($table, array_merge(
+				$this->propertyDefaults,
+				$data
+			));
+			$uid = $this->databaseConnection->sql_insert_id();
+		}
+		return $uid;
+	}
+
+	/**
+	 * Returns all known itemfields belonging to the given item and page combination,
+	 * _if_ not marked for deletion, in the format:
+	 * fieldUid => array(`field` => fieldUid,`field_value` => value)
+	 *
+	 * @param integer $itemUid
+	 * @param integer $pageUid
+	 * @return array
+	 * @throws \Innologi\Decospublisher7\Exception\SqlError
+	 */
+	protected function getItemFields($itemUid, $pageUid) {
+		$table = 'tx_decospublisher7_domain_model_itemfield';
+		$rows = $this->databaseConnection->exec_SELECTgetRows(
+			'field, field_value',
+			$table,
+			$this->databaseHelper->getWhereFromConditionArray(array(
+				'item' => $itemUid,
+				'pid' => $pageUid,
+				// if not 0, the field is no longer relevant
+				'deleted' => 0
+			), $table),
+			'',
+			'',
+			'',
+			'field'
+		);
+
+		if ($rows === NULL) {
+			throw new SqlError(array(
+				$this->databaseConnection->debug_lastBuiltQuery
+			));
+		}
+
+		return $rows;
+	}
+
+}
