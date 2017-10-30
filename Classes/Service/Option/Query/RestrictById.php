e1852cba17f7948c63124a7a101dcf9130d9a5b4
Frenck Lutke <git@innologi.nl>
2017-10-30 16:00:54 +0100

    [TASK] Add RestrictById QueryOption
    This mimics decospublisher's default behaviour of attempting to find the
    item only matching the itemID value.
    
    I would prefer to have this automated somehow, but I'm not sure how to
    do that best, as of yet. So for now, it is a query option that needs to
    be configured.
    
    Change-Id: I410462c9703d127482bb0163c86c2e8998179d2e

---
+++ Classes/Service/Option/Query/RestrictById.php
@@ -0,0 +1,72 @@
+<?php
+namespace Innologi\Decosdata\Service\Option\Query;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2017 Frenck Lutke <typo3@innologi.nl>, www.innologi.nl
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use Innologi\Decosdata\Service\Option\Exception\MissingArgument;
+use Innologi\Decosdata\Service\QueryBuilder\Query\Query;
+use Innologi\Decosdata\Service\Option\QueryOptionService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+/**
+ * RestrictById option
+ *
+ * Restricts item by its item id
+ *
+ * @package decosdata
+ * @author Frenck Lutke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class RestrictById extends OptionAbstract {
+	// @TODO it feels cumbersome having to do it like this, but doing it automatically poses other challenges.. find alternatives?
+	/**
+	 * @var \Innologi\Decosdata\Service\QueryBuilder\Query\Constraint\ConstraintFactory
+	 * @inject
+	 */
+	protected $constraintFactory;
+
+	/**
+	 * Restricts items by parent item id
+	 *
+	 * {@inheritDoc}
+	 * @see \Innologi\Decosdata\Service\Option\Query\OptionInterface::alterQueryRow()
+	 */
+	public function alterQueryRow(array $args, Query $query, QueryOptionService $service) {
+		if (!isset($args['parameter'][0])) {
+			throw new MissingArgument(1509374080, array(self::class, 'parameter'));
+		}
+		// @LOW _maybe create some service, use it as a source for every parameter-based action, including in FilterItems? otherwise every TASK applicable to FilterItems is applicable here!
+		$param = GeneralUtility::_GP('tx_decosdata_publish');
+		$itemId = rawurldecode($param[$args['parameter']]);
+
+		// @LOW so how do we solve a conflict with RestrictByParentId here?
+		$alias = 'restrictBy';
+		$parameterKey = ':' . $alias;
+		$query->getContent('itemID')->getField('')
+			->getWhere()->addConstraint(
+				$alias,
+				$this->constraintFactory->createConstraintByValue('uid', 'it', '=', $parameterKey)
+			);
+		$query->addParameter($parameterKey, $itemId);
+	}
+
+}
