326bc4ef4f4d9539f9f69569e4a1b77e3e43bf79
Frenck Lutke <git@innologi.nl>
2017-11-20 16:22:44 +0100

    [TASK] Add ImageFile renderoption
    Change-Id: Ie23871017a7e70f9a2ffcf9bba2a6ce259dfe6c9

---
+++ Classes/Service/Option/Render/ImageFile.php
@@ -0,0 +1,66 @@
+<?php
+namespace Innologi\Decosdata\Service\Option\Render;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2017 Frenck Lutke <typo3@innologi.nl>, www.innologi.nl
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use Innologi\Decosdata\Service\Option\RenderOptionService;
+use Innologi\Decosdata\Library\TagBuilder\TagInterface;
+/**
+ * Image File option
+ *
+ * Renders a file as an Image.
+ *
+ * @package decosdata
+ * @author Frenck Lutke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class ImageFile extends FileOptionAbstract {
+	// @TODO ___Absolute URIs for other contexts than normal HTML?
+
+	/**
+	 * @var \TYPO3\CMS\Extbase\Service\ImageService
+	 * @inject
+	 */
+	protected $imageService;
+
+	/**
+	 * {@inheritDoc}
+	 * @see \Innologi\Decosdata\Service\Option\Render\OptionInterface::alterContentValue()
+	 * @see \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
+	 */
+	public function alterContentValue(array $args, TagInterface $tag, RenderOptionService $service) {
+		if ( !$this->isFileHandle($service->getOriginalContent()) || ($file = $this->getFileObject($this->fileUid)) === NULL) {
+			return $tag;
+		}
+		// @LOW no check on whether it really is an image?
+		$processingInstructions = [
+			'width' => $args['width'] ?? NULL,
+			'height' => $args['height'] ?? NULL,
+		];
+		$processedImage = $this->imageService->applyProcessingInstructions($file, $processingInstructions);
+		$imageUri = $this->imageService->getImageUri($processedImage);
+
+		return $service->getTagFactory()->createTag('img', ['src' => $imageUri]);
+	}
+
+}
