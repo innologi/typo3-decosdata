249dd8fb73639492f0c2bef252276efb33f86158
Frenck Lutke <git@innologi.nl>
2018-05-02 16:23:52 +0200

    [FEATURE] Add PdfSplit RenderOption
    Splits multi-page PDF files into multiple single page files at a
    configured path, and takes a "renderOptions" argument for further
    executing options on its possibly many results.

---
+++ Classes/Service/Option/Render/PdfSplit.php
@@ -0,0 +1,165 @@
+<?php
+namespace Innologi\Decosdata\Service\Option\Render;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2017-2018 Frenck Lutke <typo3@innologi.nl>, www.innologi.nl
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use Innologi\Decosdata\Service\Option\RenderOptionService;
+use Innologi\Decosdata\Service\Option\Exception\MissingArgument;
+use Innologi\Decosdata\Service\Option\Exception\OptionException;
+use Innologi\TagBuilder\TagInterface;
+use Innologi\TagBuilder\TagContent;
+use TYPO3\CMS\Core\Resource\AbstractFile;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+/**
+ * PDF Split option
+ *
+ * Splits a multi-page PDF into multiple single page PDF files.
+ *
+ * Expects extension configuration to contain:
+ * - pdf_split_out
+ * - pdf_split_cmd
+ *
+ * @package decosdata
+ * @author Frenck Lutke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class PdfSplit implements OptionInterface {
+	use Traits\FileHandler;
+	use Traits\ExtensionConfiguration;
+
+	/**
+	 * {@inheritDoc}
+	 * @see \Innologi\Decosdata\Service\Option\Render\OptionInterface::alterContentValue()
+	 */
+	public function alterContentValue(array $args, TagInterface $tag, RenderOptionService $service) {
+		if ( ($file = $this->getFileObject($service->getOriginalContent())) === NULL || !$this->isSupportedFile($file) ) {
+			return $tag;
+		}
+
+		if (! (isset($args['renderOptions']) && is_array($args['renderOptions'])) ) {
+			throw new MissingArgument(1524141815, [self::class, 'renderOptions']);
+		}
+
+		$outputPath = PATH_site . rtrim($this->getExtensionConfiguration('pdf_split_out'), '/') . '/' . $file->getSha1() . '/';
+		$this->createDirectoryIfNotExists($outputPath);
+
+		// first check if there are already splitted files at the original file's would-be output path
+		$files = new \GlobIterator($outputPath . '*.pdf');
+		if (!$files->valid()) {
+			// if files do not exist, create them
+			$files = $this->splitPdfFile($file, $outputPath);
+		}
+
+		$content = [];
+		$i = 0;
+		/** @var \SplFileInfo $fileInfo */
+		foreach ($files as $filePath => $fileInfo) {
+			$pageIndex = $service->getIndex() . $fileInfo->getBasename('.pdf');
+			$content[] = $service->processOptions(
+				$args['renderOptions'],
+				// @FIX consider that if options fail, this may result in outputted serverpaths. in our use-case, this must never be the case
+				'mockfile:' . $filePath,
+				$pageIndex,
+				$service->getItem()
+			);
+		}
+		$content = join($args['separator'] ?? '', $content);
+
+		if ($tag instanceof TagContent) {
+			return $tag->reset()->setContent($content);
+		}
+
+		return $service->getTagFactory()->createTagContent($content);
+	}
+
+	/**
+	 * Return whether we support this file, i.e. an existing PDF
+	 *
+	 * @param AbstractFile $file
+	 * @return boolean
+	 */
+	protected function isSupportedFile(AbstractFile $file) {
+		return $file->exists() && (strpos($file->getMimeType(), '/pdf') !== FALSE);
+	}
+
+	/**
+	 * Splits PDF files by configured command
+	 *
+	 * Supports the following variables in the command:
+	 * - $OUTPUTDIR
+	 * - $INPUTFILE
+	 *
+	 * @param AbstractFile $inputFile
+	 * @param string $outputPath
+	 * @throws OptionException
+	 * @return \GlobIterator
+	 */
+	protected function splitPdfFile(AbstractFile $inputFile, $outputPath) {
+		$inputPath = PATH_site . $inputFile->getPublicUrl();
+
+		$cmd = $this->getExtensionConfiguration('pdf_split_cmd');
+		$cmd = str_replace('$OUTPUTDIR', '\'' . $outputPath . '\'', $cmd);
+		$cmd = str_replace('$INPUTFILE', '\'' . $inputPath . '\'', $cmd);
+		$cmdOutput = NULL;
+		$cmdStatus = NULL;
+		exec(escapeshellcmd($cmd), $cmdOutput, $cmdStatus);
+
+		if ($cmdStatus !== 0) {
+			// anything else is an error exit code
+			throw new OptionException(1524141893, ['Failed to run PdfSplit command.']);
+			// @LOW log $cmd + $cmdOutput + $cmdStatus
+		}
+
+		// get an iterator containing \SplFileInfo instances
+		$files = new \GlobIterator($outputPath . '*.pdf');
+		if (!$files->valid()) {
+			throw new OptionException(1524141951, ['Failed to retrieve PdfSplit output files.']);
+			// @LOW log $cmd + $cmdOutput
+		}
+
+		return $files;
+	}
+
+	/**
+	 * Checks if a local directory exists. If it doesn't, it attempts to create it one
+	 * directory at a time.
+	 *
+	 * @param string $dirpath The path to the directory
+	 * @return void
+	 */
+	protected function createDirectoryIfNotExists($dirPath) {
+		if (is_dir($dirPath)) {
+			return;
+		}
+
+		$matches = [];
+		// windows-paths are assumed to have been corrected!
+		$pattern = '=^(' . PATH_site . ')(.*)$=i';
+		// split the dirpath for use by mkdir_deep
+		preg_match($pattern, $dirPath, $matches);
+		if (GeneralUtility::mkdir_deep($matches[1], $matches[2]) !== NULL) {
+			// mkdir_deep only returns something on errors
+			// @TODO throw exception
+		}
+	}
+}
---
+++ Classes/Service/Option/Render/Traits/ExtensionConfiguration.php
@@ -0,0 +1,64 @@
+<?php
+namespace Innologi\Decosdata\Service\Option\Render\Traits;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2018 Frenck Lutke <typo3@innologi.nl>, www.innologi.nl
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use Innologi\Decosdata\Service\Option\Exception\OptionException;
+ /**
+ * Extension Configuration Trait
+ *
+ * Offers basic functionality to retrieve extension configuration.
+ *
+ * @package decosdata
+ * @author Frenck Lutke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+trait ExtensionConfiguration {
+
+	/**
+	 * @var string
+	 */
+	protected $extensionKey = 'decosdata';
+
+	/**
+	 * @var array
+	 */
+	protected $extensionConfiguration;
+
+	/**
+	 * Retrieve extension configuration by key
+	 *
+	 * @param string $key
+	 * @return array
+	 */
+	protected function getExtensionConfiguration($key) {
+		if ($this->extensionConfiguration === NULL) {
+			$this->extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$this->extensionKey]);
+		}
+		if (!isset($this->extensionConfiguration[$key])) {
+			throw new OptionException(1525269917, ['Extension Configuration key \'' . $key . '\' missing.']);
+		}
+		return $this->extensionConfiguration[$key];
+	}
+
+}
